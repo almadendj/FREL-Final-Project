7444c7d28f7ab3705070608671168013a2e05d76
almadendj <almadendj@gmail.com>
2025-05-16 10:39:30 +0800

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.computer = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // ball
+            // 
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(385, 165);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // computer
+            // 
+            this.computer.BackColor = System.Drawing.Color.Transparent;
+            this.computer.Image = ((System.Drawing.Image)(resources.GetObject("computer.Image")));
+            this.computer.Location = new System.Drawing.Point(768, 157);
+            this.computer.Name = "computer";
+            this.computer.Size = new System.Drawing.Size(20, 120);
+            this.computer.TabIndex = 1;
+            this.computer.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(12, 157);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(19, 120);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(3)))), ((int)(((byte)(11)))));
+            this.label1.Location = new System.Drawing.Point(6, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Player";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(3)))), ((int)(((byte)(11)))));
+            this.label2.Location = new System.Drawing.Point(698, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 34);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Computer";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(460, -4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(341, 47);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Agency FB", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(368, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 35);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "00:00";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 508);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.computer);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Almaden - Ping Pong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyisDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyisUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox computer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int ballXspeed = 4;
+        int ballYspeed = 4;
+        int speed = 2;
+        Random rand = new Random();
+        bool goDown, goUp;
+        int computer_speed_change = 50;
+        int playerScore = 0;
+        int computerScore = 0;
+        int playerSpeed = 8;
+        int[] computerSpeeds = { 5, 6, 8, 9 };
+        int[] ballSpeeds = { 10, 9, 8, 11, 12 };
+        int topOffset = 50;
+        int secondsElapsed = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Parent = pictureBox1;
+            label2.Parent = pictureBox2;
+            label1.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Ensure initial positions respect topOffset
+            if (ball.Top < topOffset)
+                ball.Top = topOffset;
+            if (player.Top < topOffset)
+                player.Top = topOffset;
+            if (computer.Top < topOffset)
+                computer.Top = topOffset;
+
+            // Initialize score labels
+            label1.Text = $"Player: {playerScore}";
+            label2.Text = $"Computer: {computerScore}";
+
+            // Ensure label2 (Computer) is aligned to the right
+            label2.Left = pictureBox2.Width - label2.Width - 10; // 10-pixel padding from the right
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // Adjust label2 position on form resize to keep it aligned to the right
+            label2.Left = pictureBox2.Width - label2.Width - 10;
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            ball.Top -= ballYspeed;
+            ball.Left -= ballXspeed;
+
+            // Ball boundaries: Respect topOffset for top boundary
+            if (ball.Top < topOffset || ball.Bottom > this.ClientSize.Height)
+            {
+                ballYspeed = -ballYspeed;
+            }
+
+            if (ball.Left < -2)
+            {
+                ball.Left = this.ClientSize.Width / 2;
+                ballXspeed = -ballXspeed;
+                computerScore++;
+                label2.Text = $"Computer: {computerScore}"; // Update score
+            }
+
+            if (ball.Left > this.ClientSize.Width + 2)
+            {
+                ball.Left = this.ClientSize.Width / 2;
+                ballXspeed = -ballXspeed;
+                playerScore++;
+                label1.Text = $"Player: {playerScore}"; // Update score
+            }
+
+            // Computer paddle boundaries: Respect topOffset
+            if (computer.Top < topOffset)
+            {
+                computer.Top = topOffset;
+            }
+            else if (computer.Bottom >= this.ClientSize.Height)
+            {
+                computer.Top = this.ClientSize.Height - computer.Height;
+            }
+
+            // Computer AI
+            if (ball.Top < computer.Top + (computer.Height / 2) && ball.Left > (this.ClientSize.Width / 2))
+            {
+                computer.Top -= speed;
+            }
+            if (ball.Top > computer.Top + (computer.Height / 2) && ball.Left > (this.ClientSize.Width / 2))
+            {
+                computer.Top += speed;
+            }
+
+            computer_speed_change -= 1;
+
+            if (computer_speed_change < 0)
+            {
+                speed = computerSpeeds[rand.Next(computerSpeeds.Length)];
+                computer_speed_change = 50;
+            }
+
+            // Player movement: Respect topOffset
+            if (goDown && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += playerSpeed;
+            }
+
+            if (goUp && player.Top > topOffset)
+            {
+                player.Top -= playerSpeed;
+            }
+
+            CheckCollision(ball, player, player.Right + 5);
+            CheckCollision(ball, computer, computer.Left - 35);
+
+            if (computerScore > 5)
+            {
+                GameOver("Sorry, you lost the game");
+            }
+            else if (playerScore > 5)
+            {
+                GameOver("You won!");
+            }
+        }
+
+        private void KeyisDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+        }
+
+        private void KeyisUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+        }
+
+        private void CheckCollision(PictureBox PicOne, PictureBox PicTwo, int offset)
+        {
+            if (PicOne.Bounds.IntersectsWith(PicTwo.Bounds))
+            {
+                PicOne.Left = offset;
+
+                int x = ballSpeeds[rand.Next(ballSpeeds.Length)];
+                int y = ballSpeeds[rand.Next(ballSpeeds.Length)];
+
+                if (ballXspeed < 0)
+                {
+                    ballXspeed = x;
+                }
+                else
+                {
+                    ballXspeed = -x;
+                }
+
+                if (ballYspeed < 0)
+                {
+                    ballYspeed = -y;
+                }
+                else
+                {
+                    ballYspeed = y;
+                }
+            }
+        }
+
+        private void timer1Tick(object sender, EventArgs e)
+        {
+            secondsElapsed++; // Increment seconds
+            int minutes = secondsElapsed / 60; // Calculate minutes
+            int seconds = secondsElapsed % 60; // Calculate remaining seconds
+            label3.Text = $"{minutes:D2}:{seconds:D2}"; // Update label (e.g., "01:23")
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void GameOver(string message)
+        {
+            GameTimer.Stop();
+            MessageBox.Show(message, "Game: ");
+            computerScore = 0;
+            playerScore = 0;
+            ballXspeed = ballYspeed = 4;
+            computer_speed_change = 50;
+            label1.Text = $"Player: {playerScore}"; // Reset score display
+            label2.Text = $"Computer: {computerScore}"; // Reset score display
+            GameTimer.Start();
+        }
+    }
+}
